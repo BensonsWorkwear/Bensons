92de37ce27fd7f6327450a1958c09b0becbf30fa
Julen Ortiz <Julortizw@gmail.com>
2018-03-08 12:06:58 +0000

    Filled the layouts Added missing strings in strings.xml

--- app/src/main/java/com/bensonsworkwear/bensons/NavigationActivity.java
+++ app/src/main/java/com/bensonsworkwear/bensons/NavigationActivity.java
@@ -8,6 +8,6 @@ public class NavigationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_navigation);
+        setContentView(R.layout.activity_navigation);
     }
 }
