c9ddad9c3fb2990dac346808c1f0a19c4e357809
Julen Ortiz <Julortizw@gmail.com>
2018-03-09 16:53:13 +0000

    I had deleted a line, added it as a comment just in case I end up needing it.

--- app/src/main/java/com/bensonsworkwear/bensons/constants/Constants.java
+++ app/src/main/java/com/bensonsworkwear/bensons/constants/Constants.java
@@ -2,6 +2,8 @@ package com.bensonsworkwear.bensons.constants;
 
 public class Constants {
 
+    //public static final String TAG_FRG_NAVIGATION = "Navigation";
+
     public static final String TAG_FRG_NEWS = "News";
     public static final String TAG_FRG_FEEDS = "Feeds";
     public static final String TAG_FRG_WATCH_LIVE = "Watch live";
