75c4a7edc4914cf4936825039bae104a06eebdb5
Julen Ortiz <Julortizw@gmail.com>
2018-03-09 13:29:19 +0000

    Fixed incorrect package names

--- app/src/main/java/com/bensonsworkwear/bensons/fragment/NewsFragment.java
+++ app/src/main/java/com/bensonsworkwear/bensons/fragment/NewsFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.navigationdrawerwithrecyclerview.R;
+import com.bensonsworkwear.bensons.R;
 
 import butterknife.ButterKnife;
 
