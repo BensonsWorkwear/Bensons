e46eac2bdf221698dca9aea42d3604cd938af4bc
Julen Ortiz <Julortizw@gmail.com>
2018-03-14 17:03:57 +0000

    Deleted unnecessary imports

--- app/src/main/java/com/bensonsworkwear/bensons/fragment/TestFragment.java
+++ app/src/main/java/com/bensonsworkwear/bensons/fragment/TestFragment.java
@@ -1,8 +1,6 @@
 package com.bensonsworkwear.bensons.fragment;
 
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
