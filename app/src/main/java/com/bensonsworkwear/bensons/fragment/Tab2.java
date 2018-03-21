a071fb13941bc03fa677246b35a2426474ccc920
Julen Ortiz <Julortizw@gmail.com>
2018-03-21 14:42:39 +0000

    Import for the Android Material Color Picker Dialog library

--- app/src/main/java/com/bensonsworkwear/bensons/fragment/Tab2.java
+++ app/src/main/java/com/bensonsworkwear/bensons/fragment/Tab2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
 
 import com.bensonsworkwear.bensons.R;
 
