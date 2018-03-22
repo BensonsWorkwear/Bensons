6014b64997f44da29836eb77dfb0765edc3f7339
Julen Ortiz <Julortizw@gmail.com>
2018-03-22 14:35:23 +0000

    Added material to strings.xml

--- app/src/main/java/com/bensonsworkwear/bensons/fragment/Tab2.java
+++ app/src/main/java/com/bensonsworkwear/bensons/fragment/Tab2.java
@@ -1,20 +1,51 @@
 package com.bensonsworkwear.bensons.fragment;
 
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
 
 import com.bensonsworkwear.bensons.R;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
 
 public class Tab2 extends Fragment {
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //'com.bensonsworkwear.bensons.fragment.Selection' is not an enclosing class
+        final ColorPicker cp = new ColorPicker(Selection.this, 0, 0, 0);
+
+        /* Show color picker dialog */
+        cp.show();
+
+        /* Set a new Listener called when user click "select" */
+        cp.setCallback(new ColorPickerCallback() {
+
+            @Override
+            public void onColorChosen(@ColorInt int color) {
+                // Do whatever you want
+                // Examples
+                Log.d("Alpha", Integer.toString(Color.alpha(color)));
+                Log.d("Red", Integer.toString(Color.red(color)));
+                Log.d("Green", Integer.toString(Color.green(color)));
+                Log.d("Blue", Integer.toString(Color.blue(color)));
+
+                Log.d("Pure Hex", Integer.toHexString(color));
+                Log.d("#Hex no alpha", String.format("#%06X", (0xFFFFFF & color)));
+                Log.d("#Hex with alpha", String.format("#%08X", (0xFFFFFFFF & color)));
+            }
+        });
+
         return inflater.inflate(R.layout.tab2, container, false);
     }
+
 }
